b656096db454923c7fc0ab04921f7a7099d16090
Glen Lee <dlrudrn2088@gmail.com>
2026-01-15 15:54:54 +0900

    UI: Datepicker 한글 로컬라이징 적용 (월 표시 한글화)

--- static/js/common.js
+++ static/js/common.js
@@ -1,4 +1,19 @@
 /**
+ * Datepicker 한글 설정
+ */
+$.datepicker.setDefaults({
+    prevText: '이전 달',
+    nextText: '다음 달',
+    monthNames: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
+    monthNamesShort: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
+    dayNames: ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일'],
+    dayNamesShort: ['일', '월', '화', '수', '목', '금', '토'],
+    dayNamesMin: ['일', '월', '화', '수', '목', '금', '토'],
+    showMonthAfterYear: true,
+    yearSuffix: '년'
+});
+
+/**
  * 새로운 공동명의자 정보 추가
  */
 function createNewJointInfo() {
